6570adec2d242bc3e1a5a7a4a67438909f47ce41
developiaa <developiaa@gmail.com>
2022-09-20 23:13:04 +0900

    feat: Add transaction propagation test #1

---
+++ src/main/java/study/developia/transaction/propagation/Member.java
@@ -0,0 +1,27 @@
+package study.developia.transaction.propagation;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String usernaem;
+
+    public Member(String username) {
+        this.usernaem = username;
+    }
+
+    public Member() {
+
+    }
+}
